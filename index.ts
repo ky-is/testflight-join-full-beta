7ea52c3f231a8a56123b5de2171aa56bb3d10dda
Kyle <kylecoburn@gmail.com>
2023-03-25 09:25:05 -0400

    ♻️ email helper

--- index.ts
+++ index.ts
@@ -11,6 +11,31 @@ dotenvConfig()
 
 const browser = await puppeteer.launch({ executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' })
 
+// Email
+
+async function notifyEmail(subject: string, link: string) {
+	const email = process.env.EMAIL_ADDRESS
+	console.log(subject, 'to', email)
+	const nodemailerTransporter = createTransport({
+		service: 'gmail',
+		auth: {
+			user: email,
+			pass: process.env.EMAIL_PASSWORD,
+		},
+	})
+	try {
+		await nodemailerTransporter.sendMail({
+			from: email,
+			to: email,
+			subject,
+			text: link,
+			html: `<a href="${link}">${link}</a>`,
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
+
 // Loop
 
 async function checkAvailability() {
@@ -20,31 +45,11 @@ async function checkAvailability() {
 	await page.goto(pageURL, { waitUntil: 'domcontentloaded' })
 	const results = await page.evaluate(() => {
 		const links = Array.from(document.querySelectorAll('a'))
-		return links.find(el => el.textContent?.toUpperCase() === 'START TESTING')
+		return links.find(el => el.textContent?.trim().toUpperCase() === 'START TESTING')
 	})
 	if (results != null) {
-		const email = process.env.EMAIL_ADDRESS
 		const title = (await page.title()).split(' - TestFlight')[0]
-		console.log(title, 'to', email)
-
-		const nodemailerTransporter = createTransport({
-			service: 'gmail',
-			auth: {
-				user: email,
-				pass: process.env.EMAIL_PASSWORD,
-			},
-		})
-		try {
-			await nodemailerTransporter.sendMail({
-				from: email,
-				to: email,
-				subject: title,
-				text: pageURL,
-				html: `<a href="${pageURL}">${pageURL}</a>`,
-			})
-		} catch (error) {
-			console.log(error)
-		}
+		await notifyEmail(title, pageURL)
 	} else {
 		const minutesUntilUpdate = Math.random() * (MIN_WAIT_MINUTES * MIN_WAIT_MINUTES - MIN_WAIT_MINUTES) + MIN_WAIT_MINUTES
 		setTimeout(checkAvailability, minutesUntilUpdate * 60 * 1000)
