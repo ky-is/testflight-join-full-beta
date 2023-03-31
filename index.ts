d82d77be4ff5a5bf6be122fa6721f43e6a9b99c2
Kyle <kylecoburn@gmail.com>
2023-03-31 12:02:09 -0400

    ✨ log app titles and idle time at start

--- index.ts
+++ index.ts
@@ -8,9 +8,8 @@ import type { Page } from 'puppeteer-core'
 
 dotenvConfig()
 
-const minWaitMinutes = parseInt(process.env.MIN_WAIT_MINUTES ?? '', 10) ?? 5
-
-const browser = await puppeteer.launch({ executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' })
+const minWaitMinutes = parseInt(process.env.MIN_WAIT_MINUTES ?? '5', 10)
+const rangeOfWaitMinutes = minWaitMinutes * minWaitMinutes - minWaitMinutes
 
 let remainingAppIDs = process.env.TESTFLIGHT_APP_IDS?.split(',').filter(id => id.trim().length > 0) ?? []
 
@@ -41,19 +40,20 @@ async function notifyEmail(subject: string, link: string) {
 
 // Loop
 
-async function loadTestFlightPage(appID: string) {
-	const pageURL = `https://testflight.apple.com/join/${appID}`
-	const page = await browser.newPage()
-	await page.goto(pageURL, { waitUntil: 'domcontentloaded' })
-	return [ page, pageURL ] as [Page, string]
+async function loadPages(appIDs: string[]) {
+	const browser = await puppeteer.launch({ executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' })
+	return Promise.all(appIDs.map(async appID => {
+		const pageURL = `https://testflight.apple.com/join/${appID}`
+		const page = await browser.newPage()
+		await page.goto(pageURL, { waitUntil: 'domcontentloaded' })
+		return [ appID, page, pageURL ] as [string, Page, string]
+	}))
 }
 
 async function checkAvailability() {
 	console.log(new Date().toLocaleString())
-	await Promise.all(remainingAppIDs.map(async appID => {
-		const [ page, pageURL ] = await loadTestFlightPage(appID)
+	for (const [appID, page, pageURL] of await loadPages(remainingAppIDs)) {
 		const title = (await page.title()).split(' - TestFlight')[0]
-		console.log(title.replace('Join the ', '').replace(' beta', ''), pageURL)
 		const results = await page.evaluate(() => {
 			const links = Array.from(document.querySelectorAll('a'))
 			return links.find(el => el.textContent?.trim().toUpperCase() === 'START TESTING')
@@ -62,14 +62,25 @@ async function checkAvailability() {
 			remainingAppIDs = remainingAppIDs.filter(id => id !== appID)
 			await notifyEmail(title, pageURL)
 		}
-	}))
-	if (remainingAppIDs?.length) {
-		const minutesUntilUpdate = Math.random() * (minWaitMinutes * minWaitMinutes - minWaitMinutes) + minWaitMinutes
+	}
+	if (remainingAppIDs.length) {
+		const minutesUntilUpdate = Math.random() * rangeOfWaitMinutes + minWaitMinutes
 		setTimeout(checkAvailability, minutesUntilUpdate * 60 * 1000)
 		console.log('Unavailable:', remainingAppIDs, 'Check again in:', `${Math.round(minutesUntilUpdate)} minutes.`)
 	}
 }
 
 // Run
+
+async function logTitlesForAppIDs() {
+	console.log('Checking every', minWaitMinutes, 'to', rangeOfWaitMinutes + minWaitMinutes, 'minutes:')
+	for (const [_, page, pageURL] of await loadPages(remainingAppIDs)) {
+		const title = (await page.title()).split(' - TestFlight')[0]
+		console.log(title.replace('Join the ', '').replace(' beta', ''), pageURL)
+	}
+	console.log('')
+}
+
+await logTitlesForAppIDs()
 
 checkAvailability()
